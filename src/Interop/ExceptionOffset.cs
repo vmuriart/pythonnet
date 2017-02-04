8228710c1bacfce279ab1bf4e589afe6b3e87323
Victor Uriarte <victor.m.uriarte@intel.com>
2017-02-03 22:39:51 -0700

    Define exceptionoffset

--- src/Interop/ExceptionOffset.cs
+++ src/Interop/ExceptionOffset.cs
@@ -1,6 +1,38 @@
 using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Python.Runtime.Interop
 {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    internal class ExceptionOffset
+    {
+        static ExceptionOffset()
+        {
+            Type type = typeof(ExceptionOffset);
+            FieldInfo[] fi = type.GetFields();
+            int size = IntPtr.Size;
+            for (int i = 0; i < fi.Length; i++)
+            {
+                fi[i].SetValue(null, (i * size) + ObjectOffset.ob_type + size);
+            }
+        }
+
+        // PyException_HEAD
+        // (start after PyObject_HEAD)
+        public static int dict = 0;
+        public static int args = 0;
+#if PYTHON2
+        public static int message = 0;
+#elif PYTHON3
+        public static int traceback = 0;
+        public static int context = 0;
+        public static int cause = 0;
+        public static int suppress_context = 0;
+#endif
+
+        // extra c# data
+        public static int ob_dict;
+        public static int ob_data;
+    }
 }
--- src/runtime/interop.cs
+++ src/runtime/interop.cs
@@ -133,17 +133,6 @@ namespace Python.Runtime
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     internal class ExceptionOffset
     {
-        static ExceptionOffset()
-        {
-            Type type = typeof(ExceptionOffset);
-            FieldInfo[] fi = type.GetFields();
-            int size = IntPtr.Size;
-            for (int i = 0; i < fi.Length; i++)
-            {
-                fi[i].SetValue(null, (i * size) + ObjectOffset.ob_type + size);
-            }
-        }
-
         public static int Size()
         {
             return ob_data + IntPtr.Size;
@@ -153,14 +142,11 @@ namespace Python.Runtime
         // (start after PyObject_HEAD)
         public static int dict = 0;
         public static int args = 0;
-#if PYTHON2
         public static int message = 0;
-#elif PYTHON3
         public static int traceback = 0;
         public static int context = 0;
         public static int cause = 0;
         public static int suppress_context = 0;
-#endif
 
         // extra c# data
         public static int ob_dict;

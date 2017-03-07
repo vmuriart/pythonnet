52181899eee106e7b403ae1fd43b9e53e5e94080
Victor Uriarte <victor.m.uriarte@intel.com>
2017-03-06 20:42:37 -0700

    add namespace

--- src/clrmodule/ClrModule.cs
+++ src/clrmodule/ClrModule.cs
@@ -28,99 +28,102 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using RGiesecke.DllExport;
 
-public class clrModule
+namespace Python.clr
 {
-#if PYTHON3
-    [DllExport("PyInit_clr", CallingConvention.StdCall)]
-    public static IntPtr PyInit_clr()
-#elif PYTHON2
-    [DllExport("initclr", CallingConvention.StdCall)]
-    public static void initclr()
-#endif
+    public class ClrModule
     {
-        DebugPrint("Attempting to load 'Python.Runtime' using standard binding rules.");
+#if PYTHON3
+        [DllExport("PyInit_clr", CallingConvention.StdCall)]
+        public static IntPtr PyInit_clr()
+#elif PYTHON2
+        [DllExport("initclr", CallingConvention.StdCall)]
+        public static void initclr()
+#endif
+        {
+            DebugPrint("Attempting to load 'Python.Runtime' using standard binding rules.");
 #if USE_PYTHON_RUNTIME_PUBLIC_KEY_TOKEN
-        var pythonRuntimePublicKeyTokenData = new byte[] { 0x50, 0x00, 0xfe, 0xa6, 0xcb, 0xa7, 0x02, 0xdd };
+            var pythonRuntimePublicKeyTokenData = new byte[] { 0x50, 0x00, 0xfe, 0xa6, 0xcb, 0xa7, 0x02, 0xdd };
 #endif
 
-        // Attempt to find and load Python.Runtime using standard assembly binding rules.
-        // This roughly translates into looking in order:
-        // - GAC
-        // - ApplicationBase
-        // - A PrivateBinPath under ApplicationBase
-        // With an unsigned assembly, the GAC is skipped.
-        var pythonRuntimeName = new AssemblyName("Python.Runtime")
-        {
+            // Attempt to find and load Python.Runtime using standard assembly binding rules.
+            // This roughly translates into looking in order:
+            // - GAC
+            // - ApplicationBase
+            // - A PrivateBinPath under ApplicationBase
+            // With an unsigned assembly, the GAC is skipped.
+            var pythonRuntimeName = new AssemblyName("Python.Runtime")
+            {
 #if USE_PYTHON_RUNTIME_VERSION
-            // Has no effect until SNK works. Keep updated anyways.
-            Version = new Version("2.3.0"),
+                // Has no effect until SNK works. Keep updated anyways.
+                Version = new Version("2.3.0"),
 #endif
-            CultureInfo = CultureInfo.InvariantCulture
-        };
+                CultureInfo = CultureInfo.InvariantCulture
+            };
 #if USE_PYTHON_RUNTIME_PUBLIC_KEY_TOKEN
-        pythonRuntimeName.SetPublicKeyToken(pythonRuntimePublicKeyTokenData);
+            pythonRuntimeName.SetPublicKeyToken(pythonRuntimePublicKeyTokenData);
 #endif
-        // We've got the AssemblyName with optional features; try to load it.
-        Assembly pythonRuntime;
-        try
-        {
-            pythonRuntime = Assembly.Load(pythonRuntimeName);
-            DebugPrint("Success loading 'Python.Runtime' using standard binding rules.");
-        }
-        catch (IOException)
-        {
-            DebugPrint("'Python.Runtime' not found using standard binding rules.");
+            // We've got the AssemblyName with optional features; try to load it.
+            Assembly pythonRuntime;
             try
             {
-                // If the above fails for any reason, we fallback to attempting to load "Python.Runtime.dll"
-                // from the directory this assembly is running in. "This assembly" is probably "clr.pyd",
-                // sitting somewhere in PYTHONPATH.  This is using Assembly.LoadFrom, and inherits all the
-                // caveats of that call.  See MSDN docs for details.
-                // Suzanne Cook's blog is also an excellent source of info on this:
-                // http://blogs.msdn.com/suzcook/
-                // http://blogs.msdn.com/suzcook/archive/2003/05/29/57143.aspx
-                // http://blogs.msdn.com/suzcook/archive/2003/06/13/57180.aspx
-
-                Assembly executingAssembly = Assembly.GetExecutingAssembly();
-                string assemblyDirectory = Path.GetDirectoryName(executingAssembly.Location);
-                if (assemblyDirectory == null)
-                {
-                    throw new InvalidOperationException(executingAssembly.Location);
-                }
-                string pythonRuntimeDllPath = Path.Combine(assemblyDirectory, "Python.Runtime.dll");
-                DebugPrint($"Attempting to load Python.Runtime from: '{pythonRuntimeDllPath}'.");
-                pythonRuntime = Assembly.LoadFrom(pythonRuntimeDllPath);
-                DebugPrint($"Success loading 'Python.Runtime' from: '{pythonRuntimeDllPath}'.");
+                pythonRuntime = Assembly.Load(pythonRuntimeName);
+                DebugPrint("Success loading 'Python.Runtime' using standard binding rules.");
             }
-            catch (InvalidOperationException)
+            catch (IOException)
             {
-                DebugPrint("Could not load 'Python.Runtime'.");
+                DebugPrint("'Python.Runtime' not found using standard binding rules.");
+                try
+                {
+                    // If the above fails for any reason, we fallback to attempting to load "Python.Runtime.dll"
+                    // from the directory this assembly is running in. "This assembly" is probably "clr.pyd",
+                    // sitting somewhere in PYTHONPATH.  This is using Assembly.LoadFrom, and inherits all the
+                    // caveats of that call.  See MSDN docs for details.
+                    // Suzanne Cook's blog is also an excellent source of info on this:
+                    // http://blogs.msdn.com/suzcook/
+                    // http://blogs.msdn.com/suzcook/archive/2003/05/29/57143.aspx
+                    // http://blogs.msdn.com/suzcook/archive/2003/06/13/57180.aspx
+
+                    Assembly executingAssembly = Assembly.GetExecutingAssembly();
+                    string assemblyDirectory = Path.GetDirectoryName(executingAssembly.Location);
+                    if (assemblyDirectory == null)
+                    {
+                        throw new InvalidOperationException(executingAssembly.Location);
+                    }
+                    string pythonRuntimeDllPath = Path.Combine(assemblyDirectory, "Python.Runtime.dll");
+                    DebugPrint($"Attempting to load Python.Runtime from: '{pythonRuntimeDllPath}'.");
+                    pythonRuntime = Assembly.LoadFrom(pythonRuntimeDllPath);
+                    DebugPrint($"Success loading 'Python.Runtime' from: '{pythonRuntimeDllPath}'.");
+                }
+                catch (InvalidOperationException)
+                {
+                    DebugPrint("Could not load 'Python.Runtime'.");
 #if PYTHON3
-                return IntPtr.Zero;
+                    return IntPtr.Zero;
 #elif PYTHON2
-                return;
+                    return;
 #endif
+                }
             }
+
+            // Once here, we've successfully loaded SOME version of Python.Runtime
+            // So now we get the PythonEngine and execute the InitExt method on it.
+            Type pythonEngineType = pythonRuntime.GetType("Python.Runtime.PythonEngine");
+
+#if PYTHON3
+            return (IntPtr)pythonEngineType.InvokeMember("InitExt", BindingFlags.InvokeMethod, null, null, null);
+#elif PYTHON2
+            pythonEngineType.InvokeMember("InitExt", BindingFlags.InvokeMethod, null, null, null);
+#endif
         }
 
-        // Once here, we've successfully loaded SOME version of Python.Runtime
-        // So now we get the PythonEngine and execute the InitExt method on it.
-        Type pythonEngineType = pythonRuntime.GetType("Python.Runtime.PythonEngine");
-
-#if PYTHON3
-        return (IntPtr)pythonEngineType.InvokeMember("InitExt", BindingFlags.InvokeMethod, null, null, null);
-#elif PYTHON2
-        pythonEngineType.InvokeMember("InitExt", BindingFlags.InvokeMethod, null, null, null);
-#endif
-    }
-
-    /// <summary>
-    /// Substitute for Debug.Writeline(...). Ideally we would use Debug.Writeline
-    /// but haven't been able to configure the TRACE from within Python.
-    /// </summary>
-    [Conditional("DEBUG")]
-    private static void DebugPrint(string str)
-    {
-        Console.WriteLine(str);
+        /// <summary>
+        /// Substitute for Debug.Writeline(...). Ideally we would use Debug.Writeline
+        /// but haven't been able to configure the TRACE from within Python.
+        /// </summary>
+        [Conditional("DEBUG")]
+        private static void DebugPrint(string str)
+        {
+            Console.WriteLine(str);
+        }
     }
 }

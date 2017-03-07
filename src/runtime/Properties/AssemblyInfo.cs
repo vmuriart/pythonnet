ddfd0ea48f94abaa193c6dbec5d230e043f1aa14
Victor Uriarte <victor.m.uriarte@intel.com>
2017-03-07 12:49:41 -0700

    StrongName sign assemblies

--- src/runtime/Properties/AssemblyInfo.cs
+++ src/runtime/Properties/AssemblyInfo.cs
@@ -8,4 +8,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyDefaultAlias("Python.Runtime.dll")]
 
-[assembly: InternalsVisibleTo("Python.EmbeddingTest")]
+[assembly: InternalsVisibleTo("Python.EmbeddingTest, PublicKey=" +
+                              "00240000048000009400000006020000002400005253413100040000110000005ffd8f49fb44ab" +
+                              "0641b3fd8d55e749f716e6dd901032295db641eb98ee46063cbe0d4a1d121ef0bc2af95f8a7438" +
+                              "d7a80a3531316e6b75c2dae92fb05a99f03bf7e0c03980e1c3cfb74ba690aca2f3339ef329313b" +
+                              "cc5dccced125a4ffdc4531dcef914602cd5878dc5fbb4d4c73ddfbc133f840231343e013762884" +
+                              "d6143189")]
